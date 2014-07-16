8545fab272449d33e543b657f2116e2b58c35763
haterecoil <morganandco@MorganCaron.local>
2014-07-16 21:38:26 +0200

    Passage à MySQL
    ça fonctionne pas :’(

--- server.js
+++ server.js
@@ -1,69 +1,133 @@
 // @todo Vérification de ce qui est reçu (par exemple x et y en int pour setMyPos)
-
+// @todo Utiliser mysql parce qu'on a des index pour x et y (pour récup haut à droite, utiliser les FIRIN' MUH LAZOR)
+ 
 var http = require('http'),
     fs = require('fs'),
-    // NEVER use a Sync function except at start-up!
-    index = fs.readFileSync(__dirname + '/server_index.html');
+    url = require('url'),
+    path = require('path'),
+    index = fs.readFileSync(__dirname + '/public/index.html'),
+    db = require('db.js');
+ 
+/*
+%  ROUTES =================================================
+*/
 
-// Send index.html to all requests
-var app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index);
-});
+var express = require('express');
+var app = express();
+
+//on définit nos pages comem statique et provenant de /public/
+app.use(express.static(__dirname + '/public'));
+
+// Gestion des routes
+
+//Instance de router :
+var router = express.Router();
+
+router.route('/')
+    .get(function(req,res,next){
+        res.setHeader('Content-Type', 'text/plain');
+        res.sendFile('index.html'); 
+    });
+
+router.route('/:roomName')
+    //effectue des opérations à partir du nom de la room
+    .all(function(req, res, next){
+        console.log("APP: checking roomName "+req.params.roomName);
+        if ( req.params.roomName != "favicon.ico"){
+            console.log("Un utilisateur s'est connecté sur la room "+ req.params.roomName);
+            db.connect();
+            db.checkIfRoom(req.params.roomName);
+            next();
+        }
+        else console.log("checking favicon");
+    })
+    //affiche la room
+    .get(function(req,res,next){
+        console.log("APP: checked " +req.params.roomName);
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Vous êtes sur la room '+ req.params.roomName);
+        //res.sendFile('index.html'); 
+
+    });
+
+app.use('/', router);
+app.use('/:roomName', router);
+
+
+/*
+%  SOCKET.IO=================================================
+*/
+
+var server = require('http').Server(app);
 
 // Socket.io server listens to our app
-var io = require('socket.io').listen(app);
-
-var users = []; // tableau users. users[id] = { x:, y: }
+var io = require('socket.io').listen(server);
+ 
+var users = []; // tableau users. users[id] = { x:, y:, angle: } // 0 correspond à l'angle par rapport à l'axe vertical, orienté vers le haut
 var id2socket = []; // id2socket[id] = socket;
-
+ 
 io.sockets.on('connection', function(socket) {
-	
-	var id = users.length;
-	users[id] = {x: -999, y: -999};
-	id2socket[id] = socket;
-	
-	socket.on('disconnect', function (pos)	{ delete users[id]; delete id2socket[id]; });
-	
-	socket.on('getAllPos', function (pos)	{ socket.emit('allPos',users); });
-	socket.on('setMyPos', function (pos)	{ users[id].x = pos.x; users[id].y = pos.y; console.log('setmypos');
-		console.log(users); });
-	
-	
-	// @param data = { relX, relY, message }
-	socket.on('send', function (data) {
-		console.log('send');
-		console.log(users);
-		var destX = users[id].x + data.relX;
-		var destY = users[id].y + data.relY;
-		
-		console.log('dest ' + destX + ' ' + destY);
-		
-		for (var i=0; i<users.length; i++) // @todo convert mongodb
-		{
-			console.log('i = ' + i + ' x ' + users[i].x + ' y ' + users[i].y);
-			if (users[i].x == destX && users[i].y == destY)
-			{
-				console.log('send');
-				id2socket[i].emit('message',{from: id, relX: -data.relX, relY: -data.relY, message: data.message});
-				break;
-			}
-		}
-		
-		// if (users[data.to])
-		// users[data.to].emit('get',{from: socket.pos, msg: data.msg});
-	});
+       
+        var id = users.length;
+        console.log(id);
+        users[id] = {x: -999, y: -999, angle: 0}; // angle en magnétisme 45
+        id2socket[id] = socket; // @todo transformer ça en BDD parce que ça va bientôt être galère je crois
+       
+        socket.on('disconnect', function (pos)  { 
+            users.splice(id, 1);
+            id2socket.splice(id, 1);
+            console.log('disconnection');
+            console.log(users);
+        });
+       
+        socket.on('getAllPos', function (pos)   { socket.emit('allPos',users); });
+
+        socket.on('setMyPos', function (pos)    { 
+            users[id].x = pos.x; users[id].y = pos.y; 
+            console.log('setmypos');
+            console.log(users); 
+        });
+
+        socket.on('setUser', function (name) {
+
+        }) 
+       
+       
+        // @param data = { relX, relY, message }
+        socket.on('send', function (data) {
+                console.log('send');
+                console.log(users);
+
+                var destX = users[id].x + data.relX;
+                var destY = users[id].y + data.relY;
+               
+                console.log('dest ' + destX + ' ' + destY);
+               
+                for (var i=0; i<users.length; i++) // @todo convert mongodb
+                {
+                        console.log('i = ' + i + ' x ' + users[i].x + ' y ' + users[i].y);
+                        if (users[i].x == destX && users[i].y == destY)
+                        {
+                                console.log('target locked');
+                                id2socket[i].emit('message',{from: id, relX: -data.relX, relY: -data.relY, message: data.message});
+                                break;
+                        }
+                }
+               
+                // if (users[data.to])
+                // users[data.to].emit('get',{from: socket.pos, msg: data.msg});
+        });
 });
-
-
-
-
+ 
+ 
+ 
+ 
 // on getPos
 // > sendPos
-
+ 
 // on setPos
 // > -5,-5
-
-
-
-app.listen(8081);
+ 
+ 
+ 
+server.listen(8081);
